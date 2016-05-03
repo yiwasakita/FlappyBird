8ce05392d2f50dd20d7ece81783fee191924d3d1
tlsmooth89 <tlsmooth89@gmail.com>
2016-05-03 13:42:16 +0900

    Initial Commit

---
+++ 20160503_FlappyBird_Practice01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20160503_FlappyBird_Practice01
+//
+//  Created by tlsmooth89 on 5/3/16.
+//  Copyright © 2016 yusuke.iwasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
